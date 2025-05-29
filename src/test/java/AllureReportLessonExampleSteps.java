1bad3739ff0c0b7642d2f57b07e491d39ee199bc
maja <maya-kut@mail.ru>
2025-05-29 23:29:48 +0300

    add lambda test

--- src/test/java/AllureReportLessonExampleSteps.java
+++ src/test/java/AllureReportLessonExampleSteps.java
@@ -6,17 +6,21 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
-public class AllureReportLessonExample {
+public class AllureReportLessonExampleSteps {
+    private final static String repository="eroshenkoam/allure-example";
     @Test
     public void testIssueSearch() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
+        step("Открываем главную страницу",()->{
         open("https://github.com");
-
+        });
+        step("Ищем репозиторий"+ repository);
         $(".search-input").click();
-        $("#query-builder-test").setValue("eroshenkoam/allure-example").pressEnter();
+        $("#query-builder-test").setValue(repository).pressEnter();
 
         $(linkText("eroshenkoam/allure-example")).click();
         $("#issues-tab").click();
